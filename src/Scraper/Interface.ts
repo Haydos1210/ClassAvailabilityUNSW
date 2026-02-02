6551c358210de8eb13417644c7f551144b20c722
Hayden <z5592258@ad.unsw.edu.au>
2026-02-03 02:33:20 +1100

    feat: added interface file with classInfo interface

---
+++ src/Scraper/Interface.ts
@@ -0,0 +1,9 @@
+/**
+ * @interface: Indices of all the data that can be extracted from a class chunk
+ */
+interface ClassInfo {
+  classID: number;
+  term: number;
+  status: number;
+  date: string;
+}
